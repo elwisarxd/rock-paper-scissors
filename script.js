ec18885a1bbe01ccebd0eb3d2384d0c933ba4741
elwisarxd <[200~127123485+elwisarxd@users.noreply.github.com>
2023-10-22 23:53:12 -0400

    add the duel function with the base for the score

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ let valorj1 ="";
 let Comp = "";
 let Puntosj1 = "";
 let PuntosComp = "";
-random()
+let Rondas = 0;
+
+if(Rondas === 5) {
+    marcador()
+}
+
 function start() {
     intro = prompt("te sientes con suerte?, responde con si o no.","")
     if (intro == "si") {
@@ -28,12 +33,12 @@ function random () {
          for(let i = 0; i < 4;  i++) {
             Comp = i }
     }
-    else if (Comp === 4 || Comp === 5){
+    else if (Comp === 4 || Comp === 5 || Comp === 6){
         for(let i = 0; i < 8;  i++) {
             Comp = i }
         }
     
-    else if (Comp ===7 || Comp === 8) {
+    else if (Comp ===7 || Comp === 8 || Comp === 9) {
         for(let i = 0; i < 12;  i++) {
             Comp = i }
         
@@ -48,12 +53,15 @@ function standard() {
     Eleccionj1 = prompt("haz tu eleccion, responde con piedra, papel o tijera.","")
     if (Eleccionj1 =="piedra") {
         valorj1 = 1;
+        Duelo()
     }
     else if (Eleccionj1 == "papel") {
         valorj1 = 2;
+        Duelo()
     }
-    else if (Eleccionj1 == "papel") {
+    else if (Eleccionj1 == "tijera") {
         valorj1 = 3;
+        Duelo()
     }
     else {
         alert("juego cancelado")
@@ -62,5 +70,54 @@ function standard() {
 }
 
 function Duelo () {
+    if(valorj1 === 1 && Comp === 3 ) {
+        console.log("Empate!")
+        standard()
+        Rondas++;
+    }
+    else if(valorj1 === 1 && Comp == 7) {
+        console.log("perdiste!") 
+        standard()
+        Rondas++;
+        
+    } else if(valorj1 === 1 && Comp == 11) {
+        console.log("Ganaste!")
+        standard()
+        Rondas++;
+    } 
+
+    if(valorj1 === 2 && Comp === 3 ) {
+        console.log("Ganaste!")
+        standard()
+        Rondas++;
+    }
+    else if(valorj1 === 2 && Comp == 7) {
+        console.log("Empate!") 
+        standard()
+        Rondas++;
+        
+    } else if(valorj1 === 2 && Comp == 11) {
+        console.log("Perdiste!")
+        standard()
+        Rondas++;
+    } 
+
+
+    if(valorj1 === 3 && Comp === 3 ) {
+        console.log("Perdiste!")
+        standard()
+        Rondas++;
+    }
+
+    else if(valorj1 === 3 && Comp == 7) {
+        console.log("Ganaste!") 
+        standard()
+        Rondas++;
+        
+    } else if(valorj1 === 3 && Comp == 11) {
+        console.log("Empate!")
+        standard()
+        Rondas++;
+    } 
 
 }
